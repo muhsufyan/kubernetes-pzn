926b204a787a8d98542d79accd5f1199fa57efe3
muhsufyan <muh.sufyan.ts@gmail.com>
2022-09-06 10:17:11 +0700

    done, annotation atau info tambahan

--- README.md
+++ README.md
@@ -1,27 +1,7 @@
-# label
-info tambhn like properties, metadata.<br>
-memberi tanda pada pod<br>
-mrngorganisir pod, isinya bisa versi pod, environment, dll<br>
-label can apply not just pod but also all resources in kubernetes such as replication controller, replica set, service, etc<br> 
-template pod dg label => https://github.com/khannedy/belajar-kubernetes/blob/master/templates/pod-with-label.yaml<br>
-label tdk boleh ada spasi sehingga label digunakan untuk selector (disarankan untuk selector/query/pencarian data saja)<br>
-
-menambh label maka hrs buat pod lagi (config file) => kubectl create -f namafile.yaml<br>
-melihat semua pod + label"nya => kubectl get pods --show-labels<br>
-### label digunakan untuk proses query/selector/pencarian
-#### tidak disarankan add / change label di pod yg telah ada sblmnya(lbh baik buat pod baru) karena di config file tdk akan ada add/change label tsb
-meskipun bisa saja add/change suatu pod melalui perintah :<br>
-add label => kubectl.exe label pod namapod key_label=value_label<br>
-change label => kubectl.exe label pod nginx key_label = value_label_change --overwrite<br>
-
-salah satu fungsi dr label untuk mencari/query<br>
-#see all pod dg 1 label (key)<br>
-kubectl get pods -l key_yg_dicari<br>
-kubectl get pods -l key_yg_dicari = value_yg_dicari<br>
-kubectl get pods -l '!key' #see all pods yg tdk punya label key<br>
-kubectl get pods -l key!=value<br>
-kubectl get pods -l 'key in (value1, value2)'<br>
-kubectl get pods -l 'key notin (value1,value2)'<br>
-query (mencari) pod dg beberapa label(key)<br>
-kubectl get pods key,key2=value<br>
-kubectl get pods key=value1,key2=value<br>
+# annotation
+info tmbhn dlm kubernetes (ukurannya besar, cocok untuk kebthn documentation), mirip dg label (ukurannya kecil) tp tdk bisa digunakan untuk selector filter/query<br>
+templatenya https://github.com/khannedy/belajar-kubernetes/blob/master/templates/pod-with-annotation.yaml<br>
+label & annotation ditempatkan didlm metadata<br>
+kelbhan annotation: ukurannya besar drpd label, jd boleh memasukkan keterangan tambhn apapun termasuk spasi<br>
+annotation ditulis dlm format key: value<br>
+key annotation : value annotaion<br>
--- command.sh
+++ command.sh
@@ -1,43 +1,24 @@
-# buat pod yg telah ada label-nya (sama sprti sblmnya yaitu membuat pod baru). 
-# selain itu perintah ini dignakan untuk membuat pod, service, replication controller, dll 
-kubectl create -f nginx-with-label.yaml
+# submit config file ke kubernetes master
+kubectl create -f nginx-with-annotation.yaml
 
-# see all pod & all label
+# see all pod
+kubectl get pod
+
+# show all label
 kubectl get pods --show-labels
 
-# CARA DIBAWAH INI TDK DIREKOMENDASIKAN, karena di config file tdk akan ada add/change label tsb
-# add label (formatnya)
-kubectl.exe label pod namapod key_label=value_label
+# show spesific pod with annotation
+kubectl describe pod nginx-with-annotation
+
+# add annotation in spesific pod 
+kubectl annotate pod namapod key=value
 # ex
-kubectl.exe label pod nginx-with-label environment=development
-# see label
-kubectl get pod --show-labels
-# change label (formatnya)
-kubectl.exe label pod nginx key_label = value_label_change --overwrite
+kubectl annotate pod nginx-with-annotation description="hanya boleh digunakan untuk development"
+
+# change annotation in spesific pod
+kubectl annotate pod namapod key=value --overwrite
 # ex
-kubectl.exe label pod nginx-with-label environment=testing --overwrite
+kubectl annotate pod nginx-with-annotation description="hanya tim finance yang dapat mengakses" --overwrite
 
-
-
-
-# salah satu fungsi dr label untuk mencari/query
-#see all pod dg 1 label (key)
-kubectl get pods -l key_yg_dicari
-kubectl get pods -l key_yg_dicari = value_yg_dicari
-kubectl get pods -l '!key' #see all pods yg tdk punya label key
-kubectl get pods -l key!=value
-kubectl get pods -l 'key in (value1, value2)'
-kubectl get pods -l 'key notin (value1,value2)'
-query (mencari) pod dg beberapa label(key)
-kubectl get pods key,key2=value
-kubectl get pods key=value1,key2=value
-# ex
-kubectl get pods -l environment #cari pod dimana label dg key nya environment
-kubectl get pods -l team
-kubectl get pods -l environment=production #cari pod dimana label dg key environment dan valuenya production
-kubectl get pods -l !team #cari pod dimana tidk punya label team
-kubectl get pods -l 'environment in (production, testing)' # cari pod dimana label keynya environment & valuenya production, testing
-kubectl get pods -l environment, team # cari pod dimana labelnya environment atau team
-kubectl get pods -l environment, team=finance
-
-
+# show detail spesific pod
+kubectl describe pod nginx-with-annotaion
